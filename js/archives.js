1ebb21f5a1d907db7bff60fbecb40263e6f9a599
naarmc2 <157539735+naarmc2@users.noreply.github.com>
2025-01-03 22:59:36 -0500

    colored submit button when hovered + fixed archive scrolling

--- js/archives.js
+++ js/archives.js
@@ -2,15 +2,11 @@ let  scrollContainer = document.querySelector(".gallery")
 let backBtn = document.getElementById("backBtn")
 let nextBtn = document.getElementById("nextBtn")
 
-scrollContainer.addEventListener("wheel", (evt) =>{
-  evt.preventDefault();
-  scrollContainer.scrollLeft += evt.deltaX;
-});
 
 nextBtn.addEventListener("click", ()=> {
-  scrollContainer.scrollLeft += 1600; 
+  scrollContainer.scrollLeft += 1500; 
 });
 
 backBtn.addEventListener("click", ()=> {
-  scrollContainer.scrollLeft -= 1600; 
+  scrollContainer.scrollLeft -= 1500; 
 });
--- js/general.js
+++ js/general.js
@@ -1,7 +1,7 @@
 const submitImg = document.getElementById("submitBtn");
 
 submitImg.addEventListener("mouseover", function() {
-  submitImg.src = "images/submit2.png"; // Change image source on hover
+  submitImg.src = "images/submit4.png"; // Change image source on hover
 });
 
 submitImg.addEventListener("mouseout", function() {
