6eb84f457d2e34d539718ad7df9eedc37fbb7f46
naarmc2 <157539735+naarmc2@users.noreply.github.com>
2025-02-15 21:08:11 -0500

    trying out ways to enact templating

--- js/index.js
+++ js/index.js
@@ -29,7 +29,19 @@ backButton.addEventListener('click', () => {
 
 
 
-
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = "block";
+  } else {
+      scrollToTopBtn.style.display = "none";
+  }
+});
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+      top: 0,
+      behavior: "smooth" // For smooth scrolling
+  });
+});
 
 const targetElement = document.getElementById('main');
 
